a19618d8139f90d1f2a74e966d3eb5b9ccb32601
Nicolas <nicolastimoteu@gmail.com>
2024-07-07 19:30:41 -0400

    Otimizando requisições

--- src/context/MoviesContext.tsx
+++ src/context/MoviesContext.tsx
@@ -1,39 +1,53 @@
 import React, { createContext, useState } from "react";
 
 type MoviesChildren = {
-    children: JSX.Element |JSX.Element[] 
+    children: JSX.Element | JSX.Element[]
 }
 
 export type BD = {
-    backdrop_path:string,
-    genre_ids:Array<number>,
-    id:number,
-    original_language:string,
-    overview:string,
-    popularity:number,
-    poster_path:string,
-    release_date:string,
-    title:string,
-    video:boolean
+    backdrop_path: string,
+    genre_ids: Array<number>,
+    id: number,
+    original_language: string,
+    overview: string,
+    popularity: number,
+    poster_path: string,
+    release_date: string,
+    title: string,
+    video: boolean
 }
 
-interface MoviesType  {
-    movies:Array<BD>
+interface MoviesType {
+    movies: Array<BD>
 }
 
-interface MoviesStateType extends MoviesType{
-    setMovies : React.Dispatch<React.SetStateAction<Array<BD>>>
+interface MoviesStateType extends MoviesType {
+    setMovies: React.Dispatch<React.SetStateAction<Array<BD>>>
 }
 
-export const MoviesContext = createContext<MoviesStateType|undefined>(undefined);
+export interface allGenres {
+    id: number;
+    nome: string;
+}
+
+export interface GenreType {
+    allGenres: allGenres[];
+
+}
+
+interface MoviesContext extends MoviesStateType, allGenres {
+
+}
+
+
+export const MoviesContext = createContext<MoviesStateType | undefined>(undefined);
 MoviesContext.displayName = 'movies'
 
-export function MoviesProvider ({children}:MoviesChildren){
-    const[movies, setMovies] = useState<Array<BD>>([])
+export function MoviesProvider({ children }: MoviesChildren) {
+    const [movies, setMovies] = useState<Array<BD>>([])
 
-
-    return(
-        <MoviesContext.Provider value={{movies, setMovies}}>
+    return (
+        <MoviesContext.Provider value={{ movies, setMovies }}>
             {children}
         </MoviesContext.Provider>
     )
--- src/hooks/useMoviesProvider.tsx
+++ src/hooks/useMoviesProvider.tsx
@@ -1,20 +1,21 @@
-import React, { useContext, useMemo, useState } from "react";
+import React, { useContext, useState } from "react";
 import { BD, MoviesContext } from "../context/MoviesContext";
 import { Titulo } from "../component/Titulo";
 import { Panel } from "../component/Panels/Panel";
 import { Container } from "../component/Container";
 import { nanoid } from 'nanoid'
+import allGenres from "../data/genres.json"
 import styled from "styled-components";
 
-export type allGenres = {
-    id: number
-    nome: string
-}
+// export type allGenres = {
+//     id: number
+//     nome: string
+// }
 
-export type GenreType = {
-    allGenres: allGenres[]
-    setAllGenres: React.Dispatch<React.SetStateAction<allGenres[]>>
-}
+// export type GenreType = {
+//     allGenres: allGenres[]
+//     setAllGenres: React.Dispatch<React.SetStateAction<allGenres[]>>
+// }
 
 export const NewVideoStyled = styled.div`
     display: flex;
@@ -28,6 +29,8 @@ export const NewVideoStyled = styled.div`
     align-items: baseline;
 `
 
+
+
 export const useMoviesProvider = () => {
     const context = useContext(MoviesContext)
 
@@ -38,34 +41,44 @@ export const useMoviesProvider = () => {
     const { movies, setMovies } = context
 
     const [panels, setPanels] = useState<JSX.Element | JSX.Element[]>([]);
-    const [allGenres, setAllGenres] = useState<allGenres[] | never[]>([]);
+    // const [allGenres, setAllGenres] = useState<allGenres[] | never[]>([]);
 
-    useMemo(() => {
-        async function listAllGenre() {
-            const URL = `https://my-json-server.typicode.com/limonheiro/db_genres/genres`
-            const res = await fetch(URL)
-            const data = await res.json()
-            setAllGenres(Anterior => data !== undefined ? data : Anterior)
-        }
-        listAllGenre()
-    }, [])
 
     const getGenreMovie = async (id: number) => {
-        const response = await fetch(`https://my-json-server.typicode.com/limonheiro/db_genres/genres?id=${id}`)
-            .then(response => response.json())
-            .then(data => data[0] !== undefined ? data[0].nome : "indefinido")
-            .catch(error => {
-                console.error(error)
-                return "indefinido"
-            })
-        return response
+        if (allGenres.genres.length) {
+            // console.log(allGenres)
+            const genre = allGenres.genres.filter(genre => genre.id === id)
+            if (genre.nome) {
+                // console.log(allGenres, genre)
+                return genre.nome
+            }
+
+        } else {
+            const response = await fetch(`https://my-json-server.typicode.com/limonheiro/db_genres/genres?id=${id}`)
+                .then(response => response.json())
+                .then(data => data[0] !== undefined ? data[0].nome : "indefinido")
+                .catch(error => {
+                    console.error(error)
+                    return "indefinido"
+                })
+            return response
+        }
     }
 
     const getIdGenre = async (genre: string) => {
-        const res = await fetch(`https://my-json-server.typicode.com/limonheiro/db_genres/genres?nome=${genre}`)
-        const data = await res.json()
-        const id = Number(data[0].id)
-        return `?genre_ids=${id}`
+        if (allGenres.length > 1) {
+            const genre = allGenres.filter((genre) => genre.nome === genre)
+            if (genre) {
+                // console.log(allGenres, genre)
+                return `?genre_ids=${genre.id}`
+            }
+
+        } else {
+            const res = await fetch(`https://my-json-server.typicode.com/limonheiro/db_genres/genres?nome=${genre}`)
+            const data: allGenres[] = await res.json()
+            const id = Number(data[0].id)
+            return `?genre_ids=${id}`
+        }
     }
 
     const Seccao = async (genre: string, newVideo?: true | false | undefined) => {
