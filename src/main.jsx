6af87581a4a796a467733d1fb964562643e03305
Nicolas <nicolastimoteu@gmail.com>
2024-07-10 01:11:31 -0400

    ..

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryCliente = new QueryClient
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryCliente}>
       <App />
